1a6449ab0d7480daa504cea7e6f9d9c610b7c6a1
Alexander Naumov <me@alexandernaumov.de>
2020-01-30 12:00:25 +0100

    improve dca

--- src/Resources/contao/dca/tl_translation.php
+++ src/Resources/contao/dca/tl_translation.php
@@ -1,108 +1,71 @@
 <?php
 
 $GLOBALS['TL_DCA']['tl_translation'] = [
-
     'config' => [
-
         'dataContainer' => 'Table',
-
         'sql' => [
-
             'keys' => [
-
                 'id' => [
-
                     'id' => 'primary',
+                    'name' => 'index',
                     'language' => 'index'
                 ]
             ]
         ]
     ],
-
     'list' => [
-
         'sorting' => [
-
             'mode' => 2,
             'flag' => 4,
             'fields' => [ 'language' ],
             'panelLayout' => 'filter;sort,search,limit'
         ],
-
         'label' => [
-
-            'fields' => [ 'name' ]
+            'fields' => [ 'name', 'translation' ],
+            'showColumns' => true
         ],
-
         'operations' => [
-
             'edit' => [
-
-                'label' => &$GLOBALS['TL_LANG']['tl_translation']['edit'],
                 'href' => 'act=edit',
                 'icon' => 'header.gif'
             ],
-
             'copy' => [
-
-                'label' => &$GLOBALS['TL_LANG']['tl_translation']['copy'],
                 'href' => 'act=copy',
                 'icon' => 'copy.gif'
             ],
-
             'delete' => [
-
-                'label' => &$GLOBALS['TL_LANG']['tl_translation']['delete'],
                 'href' => 'act=delete',
                 'icon' => 'delete.gif',
                 'attributes' => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
             ],
-
             'show' => [
-
-                'label' => &$GLOBALS['TL_LANG']['tl_translation']['show'],
                 'href' => 'act=show',
                 'icon' => 'show.gif'
             ]
         ],
-
         'global_operations' => [
-
             'all' => [
-
-                'label' => &$GLOBALS['TL_LANG']['MSC']['all'],
                 'href' => 'act=select',
                 'class' => 'header_edit_all',
                 'attributes' => 'onclick="Backend.getScrollOffset()" accesskey="e"'
             ]
         ]
     ],
-
     'palettes' => [
         '__selector__' => [],
         'default' => 'language,name,translation',
     ],
-
     'subpalettes' => [],
-
     'fields' => [
-
         'id' => [
-
             'sql' => ['type' => 'integer', 'autoincrement' => true, 'notnull' => true, 'unsigned' => true ]
         ],
-
         'tstamp' => [
-
             'sql' => ['type' => 'integer', 'notnull' => false, 'unsigned' => true, 'default' => 0]
         ],
-
         'language' => [
-
-            'label' =>  &$GLOBALS['TL_LANG']['tl_translation']['language'],
             'inputType' => 'select',
             'eval' => [
-
                 'chosen' => true,
                 'tl_class' => 'w50',
                 'mandatory' => true,
@@ -116,13 +79,9 @@ $GLOBALS['TL_DCA']['tl_translation'] = [
             'filter' => true,
             'sql' => ['type' => 'string', 'length' => 5, 'default' => '']
         ],
-
         'name' => [
-
-            'label' =>  &$GLOBALS['TL_LANG']['tl_translation']['name'],
             'inputType' => 'text',
             'eval' => [
-
                 'maxlength' => 64,
                 'mandatory' => true,
                 'tl_class' => 'w50'
@@ -131,18 +90,14 @@ $GLOBALS['TL_DCA']['tl_translation'] = [
             'exclude' => true,
             'sql' => ['type' => 'string', 'length' => 64, 'default' => '']
         ],
-
         'translation' => [
-
-            'label' =>  &$GLOBALS['TL_LANG']['tl_translation']['translation'],
             'inputType' => 'textarea',
             'eval' => [
-
                 'tl_class' => 'clr'
             ],
             'search' => true,
             'exclude' => true,
             'sql' => [ 'type' => 'text', 'notnull' => false ]
-        ],
+        ]
     ]
 ];
