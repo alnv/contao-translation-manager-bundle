8fe5daa45e613d06a3e8c80540525dc5c3b58dc5
Alexander Naumov <mail@alexandernaumov.de>
2020-05-09 13:22:02 +0200

    tidy up code

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -3,12 +3,9 @@
 define( "CATALOG_TRANSLATION_BUNDLE_VERSION", "1.0" );
 
 array_insert( $GLOBALS['BE_MOD']['system'], 3, [
-
     'translation-manager' => [
-
         'name' => 'translation-manager-bundle',
         'tables' => [
-
             'tl_translation'
         ]
     ]
